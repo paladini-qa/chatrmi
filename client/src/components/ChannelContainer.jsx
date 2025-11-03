7b33bd07171ddb1d87549f2dccf2089b8e7bd8d4
paladiini <paladini.qa@gmail.com>
2025-11-03 13:14:08 -0300

    first commit

---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Channel, MessageTeam } from 'stream-chat-react';
+
+import { ChannelInner, CreateChannel, EditChannel } from './';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    if(isCreating) {
+        return (
+            <div className="channel__container">
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+            </div>
+        )
+    }
+
+    if(isEditing) {
+        return (
+            <div className="channel__container">
+                <EditChannel setIsEditing={setIsEditing} />
+            </div> 
+        )
+    }
+
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">Aqui começa o Log de mensagens pessoais.</p>
+            <p className="channel-empty__second">Mande mensagens, fotos e gifs.</p>
+        </div>
+    )
+
+    return (
+        <div className=" channel__container">
+            <Channel
+                EmptyStateIndicator={EmptyState}
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+            >
+                <ChannelInner setIsEditing={setIsEditing} />
+            </Channel>
+        </div>
+    );
+}
+
+export default ChannelContainer;
